3a9178a4949958529c3f6f8d106cfd7ed11275e9
Sathiesh <Sathiesh.Kumar.Kaliyugarasan@stud.hib.no>
2018-09-11 21:47:33 +0200

    Project setup

---
+++ project_1/help_classes/Decison_tree.py
@@ -0,0 +1,5 @@
+from Tree import * 
+
+class Decision_tree(Tree):
+      def __init__(self):
+        super(Decision_tree, self).__init__()
---
+++ project_1/help_classes/Tree.py
@@ -0,0 +1,5 @@
+class Tree(object):
+    def __init__(self):
+        self.value = None
+        self.left_child = None
+        self.right_child = None
