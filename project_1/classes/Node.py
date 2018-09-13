7fb72282071444d2fb68c6216aec796fc3a5f455
Sathiesh <Sathiesh.Kumar.Kaliyugarasan@stud.hib.no>
2018-09-13 12:37:29 +0200

    working tree

--- project_1/classes/Node.py
+++ project_1/classes/Node.py
@@ -5,6 +5,8 @@ class Node(object):
     def __init__(self):
         self.data = None
         self.children = []
+        self.isChild = False
+        self.parent_node = None
 
     def add_child(self, obj):
         self.children.append(obj)
